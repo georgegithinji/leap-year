00e59d3a4a01cff9db4c07c65d57c2019fdda82c
George Githinji <georgegithinji.gg@gmail.com>
2018-02-21 17:03:49 +0300

    add the refactor function plus included test for divisibilty by 400

--- js/scripts.js
+++ js/scripts.js
@@ -45,3 +45,14 @@ $(document).ready(function() {
     };
 
     // jQuery user interface logic here.
+
+    var leapYear = function(year) {
+        if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)) {
+          return true;
+        } else {
+          return false;
+        }
+      };
+
+      // jQuery user interface logic here.
+      
